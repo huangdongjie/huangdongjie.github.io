67aef5907a8fa92fe79a8ce07a6c435e3d051c75
huang dongjie <huangdongjie95@gmail.com>
2019-01-08 23:46:43 +0800

    add even1

---
+++ themes/even1/README.md
@@ -0,0 +1,104 @@
+# hexo-theme-even
+A super simple theme for Hexo 
+
+(作者没有启用search功能,在作者基础上增加了包括站内搜索的一些小功能)
+
+[![GitHub stars](https://img.shields.io/github/stars/ahonn/hexo-theme-even.svg)](https://github.com/ahonn/hexo-theme-even/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/ahonn/hexo-theme-even.svg)](https://github.com/ahonn/hexo-theme-even/network)
+[![GitHub issues](https://img.shields.io/github/issues/ahonn/hexo-theme-even.svg)](https://github.com/ahonn/hexo-theme-even/issues)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/ahonn/hexo-theme-even/master/LICENSE)
+
+## Demo
+[原作者Demo](http://ahonn.github.io/)
+
+[现有版本](http://blog.flywinky.top)
+
+## Feature
+- 自定义样式
+- 支持 Fancybox
+- 版权信息，自定义许可协议
+- 文章打赏，添加二维码
+
+![even-screenshots](http://ouv0frko5.bkt.clouddn.com/eerqv.jpg)
+
+## 加了一点点
+- 增加来必力评论插件
+- 增加站内搜索
+- 博文可设置密码
+- 新加了几个社交图标
+
+![even-screenshots](https://s1.ax1x.com/2018/01/03/p9HFqU.png)
+
+## Installation
+```
+$ npm install hexo-renderer-scss --save
+$ git clone https://github.com/ahonn/hexo-theme-even themes/even
+```
+
+修改配置文件中的 `theme` 字段为 `even`:
+
+```
+# Extensions
+## Plugins: http://hexo.io/plugins/
+## Themes: http://hexo.io/themes/
+theme: even
+```
+
+更多主题设置，查看 [Document](https://github.com/ahonn/hexo-theme-even/wiki)
+
+## 站内搜索
+
+安装插件
+
+```bash
+npm install --save hexo-generator-search
+```
+
+安装主题后,在source新建search文件,并添加index.html,其内容如下
+ 
+```
+---
+title: 搜索
+comments: false
+---
+
+<center>
+	<span class="h1"><a href="#">站内搜索</a></span>
+	 <div id="site_search">
+        <input type="text" id="local-search-input" style="width: 200px;" results="0"/>
+        <div id="local-search-result"></div>
+    </div>
+</center>
+ ```
+ 
+ 之后在_config.yml启用站内搜索,至于各种样式可以根据下面流程设置自己喜欢的
+ 
+ [具体实现介绍](https://bayi87.github.io/2017/07/20/%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0%E7%AB%99%E5%86%85%E6%90%9C%E7%B4%A2/)
+ 
+## 博文加密
+ 
+ [安装这个插件就好了](https://github.com/MikeCoder/hexo-blog-encrypt/blob/master/ReadMe.zh.md)
+ 
+## 关于社交图标自定义方法
+ 
+ 打开[阿里巴巴矢量库](http://www.iconfont.cn/)
+ 
+ 新建自己的项目,并搜索需要的图标,生成自己的代码
+ 
+ 替换下面路径里的相应代码
+ 
+ ```
+ source\css\_partial\_iconfont.scss
+ ```
+ 
+ 并修改相对应的图标代码部分,例如下面:
+ 
+ ```
+ .icon-github:before {
+   content: "\e606"; #修改为你生成的代号
+   position: relative;
+   top: -3px;
+ }
+ ```
+ 
+[详细介绍](https://www.cnblogs.com/loveyunk/p/6222527.html)
---
+++ themes/even1/source/js/src/even.js
@@ -0,0 +1,303 @@
+(function (window) {
+  'use strict';
+
+  var Even = {};
+
+  Even.backToTop = function () {
+    var $backToTop = $('#back-to-top');
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 100) {
+        $backToTop.fadeIn(1000);
+      } else {
+        $backToTop.fadeOut(1000);
+      }
+    });
+
+    $backToTop.click(function () {
+      $('body,html').animate({ scrollTop: 0 });
+    });
+  };
+
+  Even.mobileNavbar = function () {
+    var $mobileNav = $('#mobile-navbar');
+    var $mobileNavIcon = $('.mobile-navbar-icon');
+    var slideout = new Slideout({
+      'panel': document.getElementById('mobile-panel'),
+      'menu': document.getElementById('mobile-menu'),
+      'padding': 180,
+      'tolerance': 70
+    });
+    slideout.disableTouch();
+
+    $mobileNavIcon.click(function () {
+      slideout.toggle();
+    });
+
+    slideout.on('beforeopen', function () {
+      $mobileNav.addClass('fixed-open');
+      $mobileNavIcon.addClass('icon-click').removeClass('icon-out');
+    });
+
+    slideout.on('beforeclose', function () {
+      $mobileNav.removeClass('fixed-open');
+      $mobileNavIcon.addClass('icon-out').removeClass('icon-click');
+    });
+
+    $('#mobile-panel').on('touchend', function () {
+      slideout.isOpen() && $mobileNavIcon.click();
+    });
+  };
+
+  Even.toc = function () {
+    var SPACING = 20;
+    var $toc = $('.post-toc'),
+        $footer = $('.post-footer');
+
+    if ($toc.length) {
+      var minScrollTop = $toc.offset().top - SPACING;
+      var maxScrollTop = $footer.offset().top - $toc.height() - SPACING;
+
+      var tocState = {
+        start: {
+          'position': 'absolute',
+          'top': minScrollTop
+        },
+        process: {
+          'position': 'fixed',
+          'top': SPACING
+        },
+        end: {
+          'position': 'absolute',
+          'top': maxScrollTop
+        }
+      }
+
+      $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop < minScrollTop) {
+          $toc.css(tocState.start);
+        } else if (scrollTop > maxScrollTop) {
+          $toc.css(tocState.end);
+        } else {
+          $toc.css(tocState.process);
+        }
+      })
+    }
+
+    var HEADERFIX = 30;
+    var $toclink = $('.toc-link'),
+        $headerlink = $('.headerlink');
+
+    var headerlinkTop = $.map($headerlink, function (link) {
+      return $(link).offset().top;
+    });
+
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+
+      for (var i = 0; i < $toclink.length; i++) {
+        var isLastOne = i + 1 === $toclink.length,
+            currentTop = headerlinkTop[i] - HEADERFIX,
+            nextTop = isLastOne ? Infinity : headerlinkTop[i+1] - HEADERFIX;
+
+        if (currentTop < scrollTop && scrollTop <= nextTop) {
+          $($toclink[i]).addClass('active');
+        } else {
+          $($toclink[i]).removeClass('active');
+        }
+      }
+    });
+  };
+
+  Even.fancybox = function () {
+    if ($.fancybox){
+      $('.post').each(function () {
+        $(this).find('img').each(function () {
+          $(this).wrap('<a class="fancybox" href="' + this.src + '" title="' + this.alt + '"></a>');
+        });
+      });
+
+      $('.fancybox').fancybox({
+        openEffect	: 'elastic',
+        closeEffect	: 'elastic'
+      });
+    }
+  };
+
+  Even.visits = function () {
+    var $visits = $('.post-visits');
+
+    function updateVisits(dom, time) {
+      var text = dom.text() + ' ' + time;
+      dom.text(text);
+    }
+
+    function addCounter(Counter) {
+      var query = new AV.Query(Counter);
+
+      var url = $visits.data('url').trim();
+      var title = $visits.data('title').trim();
+
+      query.equalTo('url', url);
+      query.find().then(function (results) {
+        if (results.length > 0) {
+          var counter = results[0];
+          counter.save(null, {
+            fetchWhenSave: true
+          }).then(function (counter) {
+            counter.increment('time', 1);
+            return counter.save();
+          }).then(function (counter) {
+            updateVisits($visits, counter.get('time'));
+          });
+        } else {
+          var newcounter = new Counter();
+          newcounter.set('title', title);
+          newcounter.set('url', url);
+          newcounter.set('time', 1);
+
+          newcounter.save().then(function (counter) {
+            updateVisits($visits, newcounter.get('time'));
+          });
+        }
+      }, function (error) {
+        console.log('Error:' + error.code + " " + error.message);
+      });
+    }
+
+    function showTime(Counter) {
+      $visits.each(function () {
+        var $this = $(this);
+        var query = new AV.Query(Counter);
+        var url = $this.data('url').trim();
+
+        query.equalTo('url', url);
+        query.find().then(function (results) {
+          if (results.length === 0) {
+            updateVisits($this, 0);
+          } else {
+            var counter = results[0];
+            updateVisits($this, counter.get('time'));
+          }
+        }, function (error) {
+          console.log('Error:' + error.code + " " + error.message);
+        });
+      })
+    }
+
+    if (typeof AV === 'object') {
+      var Counter = AV.Object.extend('Counter');
+      if ($visits.length === 1) {
+        addCounter(Counter);
+      } else {
+        showTime(Counter);
+      }
+    }
+  }
+
+  window.Even = Even;
+})(window);
+
+var searchFunc = function(path, search_id, content_id) {
+    'use strict'; //使用严格模式
+    $.ajax({
+        url: path,
+        dataType: "xml",
+        success: function( xmlResponse ) {
+            // 从xml中获取相应的标题等数据
+            var datas = $( "entry", xmlResponse ).map(function() {
+                return {
+                    title: $( "title", this ).text(),
+                    content: $("content",this).text(),
+                    url: $( "url" , this).text()
+                };
+            }).get();
+            //ID选择器
+            var $input = document.getElementById(search_id);
+            var $resultContent = document.getElementById(content_id);
+            function GetUrlRelativePath()
+        　　{
+        　　　　var url = document.location.toString()
+        　　　　var arrUrl = url.split("//");
+
+        　　　　var start = arrUrl[1].indexOf("/")
+        　　　　var relUrl = arrUrl[1].substring(start)
+
+        　　　　if(relUrl.indexOf("?") != -1){
+        　　　　　　relUrl = relUrl.split("?")[0]
+        　　　　}
+        　　　　return relUrl
+        　　}
+            if (GetUrlRelativePath()=='/search/'){
+                $input.addEventListener('input', function(){
+                    var str='<ul class=\"search-result-list\">';                
+                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                    $resultContent.innerHTML = "";
+                    if (this.value.trim().length <= 0) {
+                        return;
+                    }
+                    // 本地搜索主要部分
+                    datas.forEach(function(data) {
+                        var isMatch = true;
+                        var content_index = [];
+                        var data_title = data.title.trim().toLowerCase();
+                        var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                        var data_url = data.url;
+                        var index_title = -1;
+                        var index_content = -1;
+                        var first_occur = -1;
+                        // 只匹配非空文章
+                        if(data_title != '' && data_content != '') {
+                            keywords.forEach(function(keyword, i) {
+                                index_title = data_title.indexOf(keyword);
+                                index_content = data_content.indexOf(keyword);
+                                if( index_title < 0 && index_content < 0 ){
+                                    isMatch = false;
+                                } else {
+                                    if (index_content < 0) {
+                                        index_content = 0;
+                                    }
+                                    if (i == 0) {
+                                        first_occur = index_content;
+                                    }
+                                }
+                            });
+                        }
+                        // 返回搜索结果
+                        if (isMatch) {
+                        //结果标签
+                            str += "<li class='search-result-item'><a href='"+ data_url +"' class='search-result-title' target='_blank'>"+ data_title +"</a>";
+                            var content = data.content.trim().replace(/<[^>]+>/g,"");
+                            if (first_occur >= 0) {
+                                // 拿出含有搜索字的部分
+                                var start = first_occur - 5;
+                                var end = first_occur + 5;
+                                if(start < 0){
+                                    start = 0;
+                                }
+                                if(start == 0){
+                                    end = 50;
+                                }
+                                if(end > content.length){
+                                    end = content.length;
+                                }
+                                var match_content = content.substr(start, end); 
+                                // 列出搜索关键字，定义class加高亮
+                                keywords.forEach(function(keyword){
+                                    var regS = new RegExp(keyword, "gi");
+                                    match_content = match_content.replace(regS, "<span class=\"search-keyword\">"+keyword+"</span>");
+                                })
+                                str += "<p class=\"search-result\">" + match_content +"...</p></li>"
+                            }
+                        }
+                    })
+                    $resultContent.innerHTML = str;
+                })
+            }
+        }
+    })
+};
+var path = "../search.xml";
+searchFunc(path, 'local-search-input', 'local-search-result');
